49eb413966fdd93dde42f4233521b21745eb39fb
TheOlifve <hrant.hovhannisyan2413@gmail.com>
2026-06-01 16:38:18 +0400

    Bracket validator

---
+++ ex01/Brackets/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Brackets;
+
+class Program
+{
+    static bool ValidateBrackets(string input)
+    {
+        if (string.IsNullOrEmpty(input) || input.Length % 2 != 0)
+            return false;
+        
+        Stack<char> stack = new Stack<char>();
+        Dictionary<char, char> dict = new Dictionary<char, char>
+        {
+            {'{', '}'}, {'[', ']'}, {'(', ')'}
+        };
+        
+        foreach (char c in input)
+        {
+            if (dict.ContainsKey(c))
+            {
+                stack.Push(dict[c]);
+            }
+            else if (dict.ContainsValue(c))
+            {
+                if (stack.Count != 0 && stack.Pop() != c)
+                    return false;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+                return false;
+            }
+        }
+        return (stack.Count == 0);
+    }
+    
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Write("> ");
+            Console.WriteLine(ValidateBrackets(Console.ReadLine()));
+        }
+    }
+}
